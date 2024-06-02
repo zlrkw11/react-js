5d597636233e0335a6057fa5f1822ec0dfa41f67
ZL <ray040406@gmail.com>
2024-06-02 20:16:58 +1200

    notes

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,5 +4,5 @@ import React from 'react'
 //    return <h1>Hello ZL</h1>
 //}
 
-const Greet = () => <h1>Hello ZL</h1>
+const Greet = () => <h1>Hello ZL</h1> // ES6 function syntax
 export default Greet
