176aeeee0f42bea5cbd410cba2917d650821601b
ZL <ray040406@gmail.com>
2024-06-03 00:18:45 +1200

    notes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import {Greet} from './components/Greet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hello/>
         <Greet/>
         <Welcome/>
       </header>
---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello Ray</h1>
+        </div>
+    )
+}
+
+export default Hello
