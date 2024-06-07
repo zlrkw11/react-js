eedba12215bf652df5db0136680c802d667d6801
ZL <ray040406@gmail.com>
2024-06-08 01:59:56 +1200

    setting up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { Greet } from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
+import Message from "./components/Message";
+
 function App() {
   return (
     <div className="App">
---
+++ src/components/Message.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+class Message extends Component {
+  render() {
+    return <h4>Welcome visitor</h4>;
+  }
+}
+
+export default Message;
