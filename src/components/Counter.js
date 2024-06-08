0e210d0fc7ffa18b41bf09ec37caa2c248c01de8
ZL <ray040406@gmail.com>
2024-06-09 01:24:00 +1200

    arrow function passing in

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,10 +9,16 @@ class Counter extends Component {
   }
 
   increment() {
+    /* 
     this.setState({ count: this.state.count + 1 }, () => {
       console.log("Callback value", this.state.count);
     });
     console.log(this.state.count); // this runs before setState
+    */
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }))
+    console.log(this.state.count)
   }
 
   incrementFive() {
