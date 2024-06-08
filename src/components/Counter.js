ef3dfaf6716c07b589ae8a02e02a824e5ccae412
ZL <ray040406@gmail.com>
2024-06-09 01:15:03 +1200

    incrementFive

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,14 +12,22 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 }, () => {
       console.log("Callback value", this.state.count);
     });
-    console.log(this.state.count);
+    console.log(this.state.count); // this runs before setState
+  }
+
+  incrementFive() {
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
   }
 
   render() {
     return (
       <div>
         <div>count - {this.state.count}</div>
-        <button onClick={() => this.increment()}>Increment</button>
+        <button onClick={() => this.incrementFive()}>Increment</button>
       </div>
     );
   }
