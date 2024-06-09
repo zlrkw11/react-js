af319e2e23515041f29ad2e736a8dddf9cb61281
ZL <ray040406@gmail.com>
2024-06-10 03:07:28 +1200

    class component click handler

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Counter from "./components/Counter";
 import { Greet } from "./components/Greet";
 import Welcome from "./components/Welcome";
 import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+
 function App() {
   return (
     <div className="App">
       <FunctionClick />
+      <ClassClick />
     </div>
   );
 }
