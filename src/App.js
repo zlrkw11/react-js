5e02e0cfef3fb1955b17c0979fc007c5d36c7628
ZL <ray040406@gmail.com>
2024-06-10 03:13:34 +1200

    adding onclick function

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Greet } from "./components/Greet";
 import Welcome from "./components/Welcome";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
 
 function App() {
   return (
     <div className="App">
       <FunctionClick />
       <ClassClick />
+      <EventBind/>
     </div>
   );
 }
--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class EventBind extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "hello",
+    };
+  }
+
+  clickHandler() {
+    this.setState({
+      // set message to goodbye when clicked
+    });
+  }
+
   render() {
     return (
       <div>
-        
+        <div>{this.state.message}</div>
+        <button onClick={this.clickHandler}>click</button>
       </div>
-    )
+    );
   }
 }
 
-export default EventBind
+export default EventBind;
