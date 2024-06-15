667f1bb5498fb3529bc1d41cf14bed8640ce51e9
ZL <ray040406@gmail.com>
2024-06-15 19:27:06 +1200

    individual person compoent to get props

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,19 +1,21 @@
 import React from "react";
+import Person from "./Person";
 
 function NameList() {
-  const names = ["Ray", "Andy", "Nate"];
+  // const names = ["Ray", "Andy", "Nate"];
   const persons = [
     { id: 1, name: "Ray", age: 20, skill: "React" },
     { id: 2, name: "Andy", age: 20, skill: "React" },
     { id: 3, name: "Nate", age: 20, skill: "React" },
   ];
-  const nameList = names.map((name) => <h2>{name}</h2>);
+  // const nameList = names.map((name) => <h2>{name}</h2>);
+  const personList = persons.map((person) => <Person person={person} />);
   return (
     <div>
       {/*     {names.map((name) => (
         <h2>{name}</h2>
       ))}  */}
-      {nameList}
+      {personList}
     </div>
   );
 }
---
+++ src/components/Person.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Person({ person }) {
+  return (
+    <div>
+      <h2>
+        I am {person.name}, I am {person.age} years old and I know{" "}
+        {person.skill}.
+      </h2>
+    </div>
+  );
+}
+
+export default Person;
