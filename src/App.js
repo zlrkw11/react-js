e98c166b05fe98d3e913dcabfd9eaa3edf943d1f
ZL <ray040406@gmail.com>
2024-06-15 19:05:09 +1200

    set up

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
+import NameList from "./components/NameList";
 function App() {
   return (
     <div className="App">
---
+++ src/components/NameList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NameList() {
+  const names = ["Ray", "Andy", "Nate"];
+  return (
+    <div>
+      <h2>{names[0]}</h2>
+    </div>
+  );
+}
+
+export default NameList;
