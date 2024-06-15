c33d47b795241e5335852514fc47bc31a36dfae5
ZL <ray040406@gmail.com>
2024-06-15 19:15:31 +1200

    persons

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,11 +2,18 @@ import React from "react";
 
 function NameList() {
   const names = ["Ray", "Andy", "Nate"];
+  const persons = [
+    { id: 1, name: "Ray", age: 20, skill: "React" },
+    { id: 2, name: "Andy", age: 20, skill: "React" },
+    { id: 3, name: "Nate", age: 20, skill: "React" },
+  ];
+  const nameList = names.map((name) => <h2>{name}</h2>);
   return (
     <div>
-      {names.map((name) => (
+      {/*     {names.map((name) => (
         <h2>{name}</h2>
-      ))}
+      ))}  */}
+      {nameList}
     </div>
   );
 }
