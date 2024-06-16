c248157a302ae9e265f691acd9485ccf56841086
ZL <ray040406@gmail.com>
2024-06-16 18:53:19 +1200

    applied styles

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./myStyles.css";
 
 function Stylesheet() {
-  return <div><h1>Stylesheet</h1></div>;
+  return (
+    <div>
+      <h1 className="primary">Stylesheet</h1>
+    </div>
+  );
 }
 
 export default Stylesheet;
